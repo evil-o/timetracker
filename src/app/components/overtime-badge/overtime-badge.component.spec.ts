7bfb6c047bcf0ae488fd5eb070592816559c0fac
Oliver Lomp <oliver.lomp@freenet.de>
2018-02-07 19:57:06 +0100

    Fixed a display bug with rounding errors in zero overtimes.

--- src/app/components/overtime-badge/overtime-badge.component.spec.ts
+++ src/app/components/overtime-badge/overtime-badge.component.spec.ts
@@ -2,10 +2,13 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { OvertimeBadgeComponent } from './overtime-badge.component';
 import { PrecisionPipe } from '../../pipes/precision.pipe';
+import { By } from '@angular/platform-browser';
+import { DebugElement } from '@angular/core';
 
-describe('OvertimeBadgeComponent', () => {
+fdescribe('OvertimeBadgeComponent', () => {
   let component: OvertimeBadgeComponent;
   let fixture: ComponentFixture<OvertimeBadgeComponent>;
+  let span: DebugElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -21,9 +24,61 @@ describe('OvertimeBadgeComponent', () => {
     fixture = TestBed.createComponent(OvertimeBadgeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should contain a success badge for positive hours', () => {
+    component.hours = 1;
+
+    fixture.detectChanges();
+    span = fixture.debugElement.query(By.css('.badge'));
+    expect(span).toBeTruthy();
+    span = fixture.debugElement.query(By.css('.badge-success'));
+
+    expect(span).toBeTruthy();
+    expect(span.nativeElement.innerText).toContain('+1');
+  });
+
+  it('should contain a danger badge for negative hours', () => {
+    component.hours = -1;
+
+    fixture.detectChanges();
+    span = fixture.debugElement.query(By.css('.badge'));
+    expect(span).toBeTruthy();
+    span = fixture.debugElement.query(By.css('.badge-danger'));
+
+    expect(span).toBeTruthy();
+    expect(span.nativeElement.innerText).toContain('-1');
+  });
+
+  it('should show "-" for undefined hours', () => {
+    component.hours = undefined;
+
+    fixture.detectChanges();
+    span = fixture.debugElement.query(By.css('.badge'));
+    expect(span).toBeFalsy();
+
+    span = fixture.debugElement.query(By.css('span'));
+    expect(span).toBeTruthy();
+
+    expect(span.nativeElement.innerText).toContain('-');
+  });
+
+  it('should contain a neutral badge for near-zero hours', () => {
+    component.hours = 0.000001;
+
+    fixture.detectChanges();
+    span = fixture.debugElement.query(By.css('.badge'));
+    expect(span).toBeTruthy();
+
+    span = fixture.debugElement.query(By.css('.badge-default'));
+    expect(span).toBeTruthy();
+
+    expect(span.nativeElement.innerText).toContain('0');
+  });
+
 });
