0946d12c283e2748adee0a6571f03eb23eef0a5d
Oliver Lomp <ol.lomp@gmail.com>
2024-08-07 22:33:40 +0200

    refactor(ngrx): produce on helper

--- app/src/app/redux/reducers/stop-watch.reducer.ts
+++ app/src/app/redux/reducers/stop-watch.reducer.ts
@@ -1,35 +1,30 @@
-import { createReducer, on } from "@ngrx/store";
-import { producerOn } from "../../utils/ngrx";
+import { createReducer } from "@ngrx/store";
+import { produceOn } from "../../utils/ngrx";
 import { stopWatchActions } from "../actions/stop-watch.actions";
 import { StopWatch } from "../states/stopwatchState";
+
 
 export const stopWatchReducer = createReducer(
     new StopWatch(),
 
-    on(stopWatchActions.start, (state, { startTime }) => {
-        return producerOn(state, (draft) => {
-            draft.isPaused = false;
-            draft.startedAt = startTime;
-        });
+    produceOn(stopWatchActions.start, (draft, { startTime }) => {
+        draft.isPaused = false;
+        draft.startedAt = startTime;
     }),
 
-    on(stopWatchActions.pause, (state, { pauseTime }) => {
-        return producerOn(state, (draft) => {
-            let additionalTimeInMs = draft.additionalTimeInMs;
-            if (draft.startedAt) {
-                additionalTimeInMs += pauseTime.getTime() - draft.startedAt.getTime();
-            }
-            draft.isPaused = true;
-            draft.startedAt = undefined;
-            draft.additionalTimeInMs = additionalTimeInMs;
-        });
+    produceOn(stopWatchActions.pause, (draft, { pauseTime }) => {
+        let additionalTimeInMs = draft.additionalTimeInMs;
+        if (draft.startedAt) {
+            additionalTimeInMs += pauseTime.getTime() - draft.startedAt.getTime();
+        }
+        draft.isPaused = true;
+        draft.startedAt = undefined;
+        draft.additionalTimeInMs = additionalTimeInMs;
     }),
 
-    on(stopWatchActions.reset, (state) => {
-        return producerOn(state, (draft) => {
-            draft.isPaused = false;
-            draft.startedAt = undefined;
-            draft.additionalTimeInMs = 0;
-        });
+    produceOn(stopWatchActions.reset, (draft) => {
+        draft.isPaused = false;
+        draft.startedAt = undefined;
+        draft.additionalTimeInMs = 0;
     }),
 );
--- app/src/app/utils/ngrx.ts
+++ app/src/app/utils/ngrx.ts
@@ -1,5 +1,10 @@
-import { produce } from "immer";
+import { ActionCreator, ActionType, on } from "@ngrx/store";
+import { Draft, produce } from "immer";
 
-export const producerOn = <S>(state: S, producer: (draft: S) => void) => {
-    return produce(producer, state)();
+const producerOn = <S, A>(state: S, action: A, producer: (draft: Draft<S>, action: A) => void) => {
+    return produce((d) => producer(d, action), state)();
 }
+
+export const produceOn = <S, Creator extends ActionCreator>(creator: Creator, reducer: (draft: Draft<S>, action: ActionType<Creator>) => void) => on(creator, (state: S, action: ActionType<Creator>) => {
+    return producerOn(state, action, reducer);
+});
