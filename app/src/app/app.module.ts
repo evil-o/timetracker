4c47882af3df0770365da6ab84060eff4b27344c
Oliver Lomp <ol.lomp@gmail.com>
2024-08-18 21:29:48 +0200

    refactor: introduced pages and legacy modules

--- app/src/app/app.module.ts
+++ app/src/app/app.module.ts
@@ -13,8 +13,6 @@ import { StoreDevtoolsModule } from "@ngrx/store-devtools";
 
 import { environment } from "../environments/environment";
 
-import { DayComponent } from "./pages/day/day.component";
-
 import { effects } from "./redux/effects/index";
 import { metaReducers } from "./redux/metaReducers.legacy";
 import { reducers } from "./redux/reducers/index";
@@ -30,66 +28,15 @@ import { TabsModule } from "ngx-bootstrap/tabs";
 import { TypeaheadModule } from "ngx-bootstrap/typeahead";
 
 import { appRoutes } from "./app.routes";
-import { ActivityAggregationChartComponent } from "./components/activity-aggregation-chart/activity-aggregation-chart.component";
-import { ActivityColorPickerComponent } from "./components/activity-color-picker/activity-color-picker.component";
-import { ActivityLogEntryComponent } from "./components/activity-log-entry/activity-log-entry.component";
-import { ActivityPickerComponent } from "./components/activity-picker/activity-picker.component";
-import { ActivityTypeListComponent } from "./components/activity-type-list/activity-type-list.component";
-import { ActivityLogListComponent } from "./components/actvity-log-list/actvity-log-list.component";
-import { DayAttendanceComponent } from "./components/day-attendance/day-attendance.component";
-import { EditableLogEntryDescriptionComponent } from "./components/editable-log-entry-description/editable-log-entry-description.component";
-import { EditableLogEntryHoursComponent } from "./components/editable-log-entry-hours/editable-log-entry-hours.component";
-import { HourBadgeComponent } from "./components/hour-badge/hour-badge.component";
-import { NoActivityLogEntryPresentComponent } from "./components/no-activity-log-entry-present/no-activity-log-entry-present.component";
-import { OvertimeBadgeComponent } from "./components/overtime-badge/overtime-badge.component";
 import { StopwatchComponent } from "./components/stopwatch/stopwatch.component";
-import { TallyComponent } from "./components/tally/tally.component";
-import { TimeBadgeComponent } from "./components/time-badge/time-badge.component";
-import { ActivitiesComponent } from "./pages/activities/activities.component";
-import { ConfigurationComponent } from "./pages/configuration/configuration.component";
-import { StatisticsComponent } from "./pages/statistics/statistics.component";
-import { WeekComponent } from "./pages/week/week.component";
-import { WelcomeComponent } from "./pages/welcome/welcome.component";
-import { ActivityTypeIdToNamePipe } from "./pipes/activity-type-id-to-name.pipe";
-import { FormatHoursPipe } from "./pipes/format-hours.pipe";
-import { GroupActivityLogEntriesByIdPipe } from "./pipes/group-activity-log-entries-by-id.pipe";
-import { LogEntryTallyPipe } from "./pipes/log-entry-tally.pipe";
-import { PadNumberPipe } from "./pipes/pad-number.pipe";
-import { PrecisionPipe } from "./pipes/precision.pipe";
+import { LegacyModule } from "./legacy.module";
+import { PagesModule } from "./pages/pages.module";
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavbarComponent,
-        ActivityPickerComponent,
-        DayComponent,
-        ActivityLogListComponent,
-        GroupActivityLogEntriesByIdPipe,
-        WeekComponent,
-        TallyComponent,
-        LogEntryTallyPipe,
-        ActivityTypeIdToNamePipe,
-        ActivityLogEntryComponent,
-        NoActivityLogEntryPresentComponent,
-        EditableLogEntryDescriptionComponent,
-        EditableLogEntryHoursComponent,
-        WelcomeComponent,
-        HourBadgeComponent,
-        TimeBadgeComponent,
-        OvertimeBadgeComponent,
-        PrecisionPipe,
-        ActivitiesComponent,
-        ActivityTypeListComponent,
-        ActivityColorPickerComponent,
-        PadNumberPipe,
-        ConfigurationComponent,
-        DayAttendanceComponent,
-        FormatHoursPipe,
-        StatisticsComponent,
-        StopwatchComponent,
-    ],
+    declarations: [AppComponent, NavbarComponent, StopwatchComponent],
     imports: [
-        ActivityAggregationChartComponent,
+        PagesModule,
+        LegacyModule,
         BrowserModule,
         CommonModule,
         FormsModule,
---
+++ app/src/app/pages/pages.module.ts
@@ -0,0 +1,36 @@
+import { CommonModule } from "@angular/common";
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
+import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { TabsModule } from "ngx-bootstrap/tabs";
+import { LegacyModule } from "../legacy.module";
+import { ActivitiesComponent } from "./activities/activities.component";
+import { ConfigurationComponent } from "./configuration/configuration.component";
+import { DayComponent } from "./day/day.component";
+import { StatisticsComponent } from "./statistics/statistics.component";
+import { WeekComponent } from "./week/week.component";
+import { WelcomeComponent } from "./welcome/welcome.component";
+
+const pages = [
+    ActivitiesComponent,
+    ConfigurationComponent,
+    DayComponent,
+    StatisticsComponent,
+    WeekComponent,
+    WelcomeComponent,
+];
+
+@NgModule({
+    declarations: [...pages],
+    exports: [...pages],
+    imports: [
+        BrowserModule,
+        BsDatepickerModule,
+        CommonModule,
+        LegacyModule,
+        TabsModule,
+        RouterModule,
+    ],
+})
+export class PagesModule {}
