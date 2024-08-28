bbb08eec3a7abff034ab221c658ca898ed9f53f0
Oliver Lomp <ol.lomp@gmail.com>
2024-08-28 20:36:06 +0200

    test: added stopwatch cypress spec

--- testing/cypress/pages/global.page.ts
+++ testing/cypress/pages/global.page.ts
@@ -17,4 +17,30 @@ export class GlobalPage {
     public get exportData(): Cypress.Chainable<JQuery<HTMLElement>> {
         return cy.byTestId("export-data");
     }
+
+    public stopwatch = {
+        get elapsedTime(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("time-elapsed");
+        },
+
+        get start(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("start-stopwatch");
+        },
+
+        get pause(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("pause-stopwatch");
+        },
+
+        get reset(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("reset-stopwatch");
+        },
+
+        get activityPicker(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("pick-stopwatch-activity");
+        },
+
+        get logTime(): Cypress.Chainable<JQuery<HTMLElement>> {
+            return cy.byTestId("stopwatch-log");
+        },
+    };
 }
