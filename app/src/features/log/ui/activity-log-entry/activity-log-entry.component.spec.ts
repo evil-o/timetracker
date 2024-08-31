e611702deb44b0b17073b90417b77643c68ff952
Oliver Lomp <ol.lomp@gmail.com>
2024-08-31 16:23:32 +0200

    refactor: spectator for activity log component spec

--- app/src/features/log/ui/activity-log-entry/activity-log-entry.component.spec.ts
+++ app/src/features/log/ui/activity-log-entry/activity-log-entry.component.spec.ts
@@ -1,47 +1,20 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
-import { provideNoopAnimations } from "@angular/platform-browser/animations";
-import { StoreModule } from "@ngrx/store";
-import { AccordionModule } from "ngx-bootstrap/accordion";
-import { reducers } from "../../../../app/model/reducers";
-import {
-    EditableLogEntryDescriptionComponent,
-    EditableLogEntryHoursComponent,
-} from "../../../../entities/activity-log";
-import { ActivityTypeIdToNamePipe } from "../../../../entities/activity-type";
-import { metaReducers } from "../../../../entities/application/meta-reducers";
-import { FormatHoursPipe } from "../../../../shared/lib";
-import { HourBadgeComponent } from "../../../../shared/ui";
+import { createComponentFactory, Spectator } from "@ngneat/spectator";
+import { provideMockStore } from "@ngrx/store/testing";
 import { ActivityLogEntryComponent } from "./activity-log-entry.component";
 
-describe("ActivityLogEntryComponent", () => {
-    let component: ActivityLogEntryComponent;
-    let fixture: ComponentFixture<ActivityLogEntryComponent>;
-
-    beforeEach(() => {
-        TestBed.configureTestingModule({
-            declarations: [
-                ActivityLogEntryComponent,
-                ActivityTypeIdToNamePipe,
-                FormatHoursPipe,
-                HourBadgeComponent,
-                EditableLogEntryDescriptionComponent,
-                EditableLogEntryHoursComponent,
-            ],
-            imports: [
-                AccordionModule.forRoot(),
-                StoreModule.forRoot(reducers, { metaReducers }),
-            ],
-            providers: [provideNoopAnimations()],
-        }).compileComponents();
+describe(ActivityLogEntryComponent.name, () => {
+    const create = createComponentFactory({
+        component: ActivityLogEntryComponent,
+        shallow: true,
+        providers: [provideMockStore()],
     });
+    let spectator: Spectator<ActivityLogEntryComponent>;
 
     beforeEach(() => {
-        fixture = TestBed.createComponent(ActivityLogEntryComponent);
-        component = fixture.componentInstance;
-        fixture.detectChanges();
+        spectator = create();
     });
 
     it("should create", () => {
-        expect(component).toBeTruthy();
+        expect(spectator.component).toBeTruthy();
     });
 });
