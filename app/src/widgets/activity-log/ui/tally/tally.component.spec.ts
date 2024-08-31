514d83290f07dd5cbcc67b04a3cf31b1dc91163a
Oliver Lomp <ol.lomp@gmail.com>
2024-08-31 18:12:22 +0200

    test: make tally component spec shallow

--- app/src/widgets/activity-log/ui/tally/tally.component.spec.ts
+++ app/src/widgets/activity-log/ui/tally/tally.component.spec.ts
@@ -2,7 +2,10 @@ import { createComponentFactory, Spectator } from "@ngneat/spectator";
 import { TallyComponent } from "./tally.component";
 
 describe(TallyComponent.name, () => {
-    const create = createComponentFactory({ component: TallyComponent });
+    const create = createComponentFactory({
+        component: TallyComponent,
+        shallow: true,
+    });
     let spectator: Spectator<TallyComponent>;
 
     beforeEach(() => {
