062cc16f6706368d398d2bc2ed28051f6821d4e4
Oliver Lomp <ol.lomp@gmail.com>
2024-09-03 21:34:14 +0200

    refactor: added modal header

--- app/src/shared/shared.module.ts
+++ app/src/shared/shared.module.ts
@@ -1,14 +1,20 @@
 import { NgModule } from "@angular/core";
 import { BsDatepickerModule } from "ngx-bootstrap/datepicker";
+import { ModalModule } from "ngx-bootstrap/modal";
 import { PadNumberPipe } from "./lib";
 import { FormatHoursPipe } from "./lib/format-hours.pipe";
 import { PrecisionPipe } from "./lib/precision.pipe";
-import { HourBadgeComponent, TimeBadgeComponent } from "./ui";
-import { QuickDayPickerComponent } from "./ui/quick-day-picker/quick-day-picker";
+import {
+    HourBadgeComponent,
+    ModalHeaderComponent,
+    QuickDayPickerComponent,
+    TimeBadgeComponent,
+} from "./ui";
 
 const pipes = [FormatHoursPipe, PrecisionPipe, PadNumberPipe];
 const components = [
     HourBadgeComponent,
+    ModalHeaderComponent,
     QuickDayPickerComponent,
     TimeBadgeComponent,
 ];
@@ -16,6 +22,6 @@ const components = [
 @NgModule({
     declarations: [...components, ...pipes],
     exports: [...components, ...pipes],
-    imports: [BsDatepickerModule],
+    imports: [BsDatepickerModule, ModalModule],
 })
 export class SharedModule {}
--- app/src/shared/ui/index.ts
+++ app/src/shared/ui/index.ts
@@ -1,3 +1,4 @@
 export * from "./hour-badge/hour-badge.component";
+export * from "./modal-header/modal-header.component";
 export * from "./quick-day-picker/quick-day-picker";
 export * from "./time-badge/time-badge.component";
